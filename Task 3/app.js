eed18cea6adf8e10bd7a5af34ba2f27469c884d7
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2023-12-21 18:08:21 +0100

    nice css

--- Task 3/app.js
+++ Task 3/app.js
@@ -36,6 +36,25 @@ app.get('/movies', (req, res) => {
   handle_query(input_url)
 });
 
+//root to search.html
+app.get('/search', (req, res) => {
+  let input_url =  "https://solid.interactions.ics.unisg.ch/seboes/movies/"
+  let input_query = req.query.query
+  console.log("input url: ", input_url)
+  console.log("input query: ", input_query)
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, 'search.html'));
+  if (input_query == "") {
+    console.log("handeling null query")
+    handle_query(input_url)
+  } else {
+    console.log("handeling query")
+    handle_search(input_url, input_query)
+  }
+});
+
+
 async function handle_query(input_url) {
   let movie_urls = [] //List of movies in the pod
   engine.queryBindings(`SELECT ?v WHERE { ?container <http://www.w3.org/ns/ldp#contains> ?v . }`, { //TODO: complete SPARQL query
@@ -61,35 +80,37 @@ async function handle_query(input_url) {
   });
 }
 
-/*
+// function to handle search queries when the user enters a query
+async function handle_search(input_url, input_query) {
+  let movie_urls = []; // List of movies in the pod
 
-async function handle_custom_query(input_url) {
-  let movie_urls = [] //List of movies in the pod
-  engine.queryBindings(`SELECT ?v WHERE { ?container <http://www.w3.org/ns/ldp#contains> ?v . }`, { //TODO: complete SPARQL query
+  // Query to fetch movie URLs based on input URL
+  await engine.queryBindings(`SELECT ?v WHERE { ?container <http://www.w3.org/ns/ldp#contains> ?v . }`, {
     sources: [input_url],
   }).then(function (bindingsStream) {
     bindingsStream.on('data', function (data) {
-      movie_urls.push(data.get('v').value) //Variable ?v in the SPARQL query.
+      movie_urls.push(data.get('v').value); // Variable ?v in the SPARQL query.
     });
-    bindingsStream.on('end', function () {
-      engine.queryBindings(`SELECT ?name ?image WHERE { ?movie <https://schema.org/name> ?name. ?movie <https://schema.org/image> ?image.}`, { //TODO: complete SPARQL query
-        sources: movie_urls,
-      }).then(function (bindingsStream) {
-        bindingsStream.on('data', function (data) {
-          let obj = {
-            "name": data.get('name').value, //Variable ?name in the SPARQL query.
-            "image": data.get('image').value //Variable ?image in the SPARQL query.
-          };
-          io.emit('update', { 'message': obj }) //Send information to the browser.
-        });
-      });
-    })
-
   });
 
+  // Query to fetch movie names and images based on movie URLs and input query
+  await engine.queryBindings(`SELECT ?name ?image WHERE { 
+    ?movie <https://schema.org/name> ?name. 
+    ?movie <https://schema.org/image> ?image.
+    FILTER(contains(lcase(?name), lcase("${input_query}"))) 
+  }`, {
+    sources: movie_urls,
+  }).then(function (bindingsStream) {
+    bindingsStream.on('data', function (data) {
+      let obj = {
+        "name": data.get('name').value, // Variable ?name in the SPARQL query.
+        "image": data.get('image').value // Variable ?image in the SPARQL query.
+      };
+      console.log("sending: ", obj);
+      io.emit('update', { 'message': obj }); // Send information to the browser.
+    });
+  });
 }
-
-*/
 
 httpServer.listen(port),
   () => console.log("Server is running... on " + port);
